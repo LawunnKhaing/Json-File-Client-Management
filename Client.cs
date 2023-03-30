928bf1a48ff706d27970d1f4fc24aed625e7ac43
Lawunn Khaing <elakh001@edu.xamk.fi>
2023-03-30 13:13:06 +0300

    Add project files.

---
+++ Client.cs
@@ -0,0 +1,141 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPTask12B
+{
+    internal class Client
+    {
+        public Client(string? name, DateTime? dateOfRegistration)
+        {
+            Name = name;
+            DateOfRegistration = dateOfRegistration;
+        }
+
+        public string? Name { get; set; }
+        public DateTime? DateOfRegistration { get; set; }
+
+        //Instance methods here;
+        public void ModifyInfo(string path, string name)
+        {
+            bool ifChanged = false;
+
+            //Deserialize the JSON data here:
+            List<Client> clients = new List<Client>();
+
+            using (StreamReader sr = new StreamReader(path))
+            {
+                var jsonString = sr.ReadToEnd();
+                //Deserialize into generic collection of type List<Client>
+                clients = JsonConvert.DeserializeObject<List<Client>>(jsonString);
+            }
+            var chosen = from cli in clients
+                         where cli.Name.ToLower().Contains(name)
+                         select cli;
+
+            int numberOfClients = clients.Count();
+            if (chosen.Any())
+            {
+                for (int i = 0; i < numberOfClients; i++)
+                {
+                    if (clients[i].Name.ToLower().Contains(name))
+                    {
+                        Console.WriteLine("Found client {0}", clients[i].Name);
+                        Console.WriteLine("Want to change the information Y/N: ");
+                        string choice = Console.ReadLine().ToUpper();
+                        if (choice.StartsWith("Y"))
+                        {
+                            Console.WriteLine("We go through all the information.One at a time");
+                            Console.WriteLine("If you do not want to change, press ENTER");
+                            Console.WriteLine("Current name is {0}.", clients[i].Name);
+                            Console.Write("Enter new name: ");
+                            string NewName = Console.ReadLine();
+                            if (!String.IsNullOrEmpty(NewName))
+                            {
+                                clients[i].Name = NewName;
+                                ifChanged = true;
+                            }
+
+                            DateTime newDateOfRegistration;
+                            Console.WriteLine("Current date of registration is {0}.", clients[i].DateOfRegistration?.ToString("d"));
+                            Console.Write("Enter new Date of registration: ");
+                            string received = Console.ReadLine();
+                            if (!String.IsNullOrEmpty(received))
+                            {
+                                while (!DateTime.TryParse(received, out newDateOfRegistration))
+                                {
+                                    Console.Write("Not valid, try again: ");
+                                    received = Console.ReadLine();
+                                }
+                                clients[i].DateOfRegistration = newDateOfRegistration;
+                                ifChanged = true;
+                            }
+                        }
+                    }
+                }
+                if (!ifChanged)
+                    Console.WriteLine("You selected the change option, but no changes were made");
+            }
+            else
+            {
+                Console.WriteLine("No client with that name");
+            }
+            if (ifChanged)
+            {
+                Console.WriteLine("Need to save information into the file!");
+                //Serializing the collection information into your file:
+                using (StreamWriter sr = new StreamWriter(path, false))
+                {
+                    //Serializing business first
+                    string jsonData = JsonConvert.SerializeObject(clients);
+                    sr.Write(jsonData);
+                }
+            }
+        }
+        public void PrintTheContents(string path)
+        {
+            //Deserialize the JSON data here:
+            List<Client> clients = new List<Client>();
+
+            using (StreamReader sr = new StreamReader(path))
+            {
+                var jsonString = sr.ReadToEnd();
+                //Deserialize into generic collection of type List<Client>
+                clients = JsonConvert.DeserializeObject<List<Client>>(jsonString);
+            }
+
+            foreach (var item in clients)
+            {
+                Console.WriteLine("Name:{0}, date of registration: {1}", item.Name,
+                    item.DateOfRegistration?.ToString("d"));
+                //Console.WriteLine("Name:{0}, date of registration: {1}", item.Name, item.DateOfRegistration.Value.ToString("d"));
+            }
+        }
+        public void AddClient(string path, Client newOne)
+        {
+            //Deserialize the JSON data here:
+            List<Client> clients = new List<Client>();
+
+            using (StreamReader sr = new StreamReader(path))
+            {
+                var jsonString = sr.ReadToEnd();
+                //Deserialize into generic collection of type List<Client>
+                clients = JsonConvert.DeserializeObject<List<Client>>(jsonString);
+            }
+            clients.Add(newOne);
+            //Serializing the collection information into your file:
+            using (StreamWriter sr = new StreamWriter(path, false))
+            {
+                //Serializing business first
+                string jsonData = JsonConvert.SerializeObject(clients);
+                sr.Write(jsonData);
+            }
+        }
+        
+    }
+}
+    
---
+++ Program.cs
@@ -0,0 +1,123 @@
+﻿using OOPTask12B;
+using System.Diagnostics;
+
+bool more = false;
+int choice;
+string filePath;
+string clientName;
+DateTime dateOfRegistration;
+Client client = new Client(null, null);
+//File is here
+// C:\Users\95996\Downloads\client.json
+
+do
+{
+    Console.WriteLine();
+    Console.WriteLine("Welcome to JSON file processiing. Select from (1...4)");
+    Console.WriteLine("1. Output the contents.");
+    Console.WriteLine("2. Add a new client");
+    Console.WriteLine("3. Modify client information.");
+    Console.WriteLine("4. Remove client");
+    Console.WriteLine();
+    Console.Write("Select an option: ");
+    string received=Console.ReadLine();
+    while (!Int32.TryParse(received, out choice) || choice < 1 || choice>4 )
+    {
+        Console.WriteLine("Not accepted, try again");
+        received= Console.ReadLine();
+    }
+
+    switch (choice)
+    {
+        case 1:
+            Console.WriteLine("You selected: 1. Output the contents");
+            do
+            {
+                Console.Write("Enter the file path: ");
+                filePath = Console.ReadLine();
+                if (string.IsNullOrEmpty(filePath))
+                    Console.WriteLine("The field cannot be empty.");
+
+
+            } while (string.IsNullOrEmpty(filePath));
+            //Call your method now:
+            client.PrintTheContents(filePath);
+
+
+            break; 
+        
+        case 2:
+            Console.WriteLine("You selected: 2. Add a new client");
+            do
+            {
+                Console.Write("Enter the file path: ");
+                filePath = Console.ReadLine();
+                if (string.IsNullOrEmpty(filePath))
+                    Console.WriteLine("The field cannot be empty.");
+
+
+            } while (string.IsNullOrEmpty(filePath));
+            do
+            {
+                Console.Write("Enter the client name: ");
+                clientName = Console.ReadLine();
+                if (string.IsNullOrEmpty(clientName))
+                    Console.WriteLine("The field cannot be empty.");
+
+
+            } while (string.IsNullOrEmpty(clientName));
+
+            Console.WriteLine("Enter the date of registration: ");
+            received = Console.ReadLine();
+                while (!DateTime.TryParse(received, out dateOfRegistration))
+                {
+                    Console.Write("Not valid, try again: ");
+                    received = Console.ReadLine();
+                }
+            Client newClient = new Client(clientName, dateOfRegistration);
+            //Call your method now:
+            client.AddClient(filePath, newClient);
+
+
+            break;
+        case 3:
+            Console.WriteLine("You selected: 3. Modify client information");
+            do
+            {
+                Console.Write("Enter the file path: ");
+                filePath= Console.ReadLine();
+                if (string.IsNullOrEmpty(filePath))
+                    Console.WriteLine("The field cannot be empty.");
+
+
+            } while (string.IsNullOrEmpty(filePath));
+
+            do
+            {
+                Console.Write("Enter the client name: ");
+                clientName = Console.ReadLine();
+                if (string.IsNullOrEmpty(clientName))
+                    Console.WriteLine("The field cannot be empty.");
+
+
+            } while (string.IsNullOrEmpty(clientName));
+            //Call your method now:
+            client.ModifyInfo(filePath, clientName);
+            break;
+        case 4:
+            Console.WriteLine("You selected: 4. Remove client");
+            break;
+        default:
+            Console.WriteLine("Sorry, you will NEVER be here.");
+            break;
+    }
+
+    Console.Write("More operations Y/N: ");
+    received= Console.ReadLine().ToUpper();
+    if(received.StartsWith("Y"))
+        more= true;
+    else
+        more= false;
+
+
+} while (more);
